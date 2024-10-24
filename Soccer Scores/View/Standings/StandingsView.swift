b7d9ee449e073aa7d45aa25b595b0258bd0fa812
hoover13 <hoovermtk25@gmail.com>
2024-10-24 00:56:21 -0400

    - another commit

---
+++ Soccer Scores/View/Standings/StandingsView.swift
@@ -0,0 +1,33 @@
+//
+//  StandingsView.swift
+//  Soccer Scores
+//
+//  Created by Min Thu Khine on 10/24/24.
+//
+
+import SwiftUI
+
+struct StandingsView: View {
+    var body: some View {
+        
+        VStack {
+            HStack {
+                Text("Standings")
+                    .font(.largeTitle)
+                    .bold()
+                Spacer()
+                Image("Premire League")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+            }
+            .padding()
+            
+            
+            
+        }
+    }
+}
+
+#Preview {
+    StandingsView()
+}
