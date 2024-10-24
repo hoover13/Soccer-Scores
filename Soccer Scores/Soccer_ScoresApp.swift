fade9519b1d10bbc4811788e04b9420adc52eeb0
hoover13 <hoovermtk25@gmail.com>
2024-10-23 23:52:37 -0400

    Initial Commit

---
+++ Soccer Scores/Soccer_ScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Soccer_ScoresApp.swift
+//  Soccer Scores
+//
+//  Created by Min Thu Khine on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Soccer_ScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
