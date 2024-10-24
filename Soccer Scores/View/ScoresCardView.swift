e1c0dc837e2e6b61a1d8219542cbf12bddbbadcf
hoover13 <hoovermtk25@gmail.com>
2024-10-24 00:50:17 -0400

    - Implement Segmented Control with Yesterday, Today and Tomorrow - Put dummy Data and Photos

--- Soccer Scores/View/ScoresCardView.swift
+++ Soccer Scores/View/ScoresCardView.swift
@@ -13,7 +13,7 @@ struct ScoresCardView: View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
                 .fill(Color(red: 236/255, green: 242/255, blue: 245/255))
-                .shadow(radius: 20)
+                .shadow(radius: 10)
                 .padding()
             
             VStack {
